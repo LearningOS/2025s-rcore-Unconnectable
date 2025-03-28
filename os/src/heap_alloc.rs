d95e0cca8dc54073c0f52e113a4baad9e47bb49c
Phrink <scpr7@outlook.com>
2025-03-28 11:04:10 +0800

    修改了buddy_sys_allocator版本

--- os/src/heap_alloc.rs
+++ os/src/heap_alloc.rs
@@ -5,7 +5,8 @@ use buddy_system_allocator::LockedHeap;
 
 #[global_allocator]
 /// heap allocator instance
-static HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
+static HEAP_ALLOCATOR: LockedHeap<33> = LockedHeap::empty();
+
 
 /// heap space ([u8; KERNEL_HEAP_SIZE])
 static mut HEAP_SPACE: [u8; KERNEL_HEAP_SIZE] = [0; KERNEL_HEAP_SIZE];
